3f44b239d3682886e0b3e3924e856c58f27ce44c
AmarinoLama <amarinolama@gmail.com>
2024-03-12 14:08:53 +0100

    feat (conductor) : add class conductor

--- Cotxox/src/main/java/edu/badpals/Cotxox/conductores/Conductor.java
+++ Cotxox/src/main/java/edu/badpals/Cotxox/conductores/Conductor.java
@@ -1,4 +1,69 @@
 package edu.badpals.Cotxox.conductores;
 
+import java.util.ArrayList;
+
 public class Conductor {
+
+    private String nombre = null;
+    private String modelo = null;
+    private String matricula = null;
+    private double valoracionMedia = 0d;
+    private boolean ocupado = false;
+    private ArrayList<Byte> valoraciones = new ArrayList<>();
+
+    public Conductor() {
+    }
+
+    public Conductor(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public double getValoracion() {
+        return this.valoracionMedia;
+    }
+
+
+    public int getNumeroValoraciones() {
+        return this.valoraciones.size();
+    }
+
+    public void setValoracion(Byte valoracion) {
+
+    }
+    private double calcularValoracionMedia() {
+        int sumaValoraciones = 0;
+        for (byte valoracion : this.valoraciones) {
+            sumaValoraciones += valoracion;
+        }
+        this.valoracionMedia = (double) sumaValoraciones / this.valoraciones.size();
+        return this.valoracionMedia;
+    }
+
+    public void setOcupado(boolean ocupado) {
+        this.ocupado = ocupado;
+    }
+
+    public boolean isOcupado() {
+        return this.ocupado;
+    }
 }
