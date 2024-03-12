5ef449b25e8f0724566a1ad372fafd0131f28665
AmarinoLama <amarinolama@gmail.com>
2024-03-12 22:43:47 +0100

    feat (PoolConductores) : add class

--- Cotxox/src/main/java/edu/badpals/Cotxox/conductores/PoolConductores.java
+++ Cotxox/src/main/java/edu/badpals/Cotxox/conductores/PoolConductores.java
@@ -1,4 +1,30 @@
 package edu.badpals.Cotxox.conductores;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class PoolConductores {
+
+    private List<Conductor> poolConductores = new ArrayList<Conductor>();
+
+
+    public PoolConductores(List<Conductor> poolConductores) {
+        this.poolConductores = poolConductores;
+    }
+
+    public List<Conductor> getPoolConductores() {
+        return poolConductores;
+    }
+
+    public Conductor asignarConductor() {
+        Conductor conductor = new Conductor();
+        for (Conductor posicion : getPoolConductores()) {
+            if (!posicion.isOcupado()) {
+                conductor.setOcupado(true);
+                break;
+            }
+        }
+        return conductor;
+    }
 }
