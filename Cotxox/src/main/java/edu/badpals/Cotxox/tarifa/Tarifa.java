3095cdde770e2f6f9c64af77ff5a3324885ddbe3
AmarinoLama <amarinolama@gmail.com>
2024-03-12 22:43:56 +0100

    feat (Tarifa) : add class

--- Cotxox/src/main/java/edu/badpals/Cotxox/tarifa/Tarifa.java
+++ Cotxox/src/main/java/edu/badpals/Cotxox/tarifa/Tarifa.java
@@ -1,4 +1,26 @@
 package edu.badpals.Cotxox.tarifa;
 
+import edu.badpals.Cotxox.carrera.Carrera;
+
 public class Tarifa {
+    final static double COSTA_MILLA = 1.35;
+    final static double COSTE_MINUTO = 0.35;
+    final static double COSTE_MINIMO = 5.0;
+    final static byte PORCENTAJE_COMISION = 20;
+
+    public Tarifa() {
+    }
+
+    public static double getCosteDistancia(double distancia) {
+        return distancia * COSTA_MILLA;
+    }
+
+    public static double getCosteTiempo(int tiempo) {
+        return tiempo * COSTE_MINUTO;
+    }
+
+    public static double getCosteTotalEsperado(Carrera carrera) {
+        double costeTotal = getCosteDistancia(carrera.getDistancia()) + getCosteTiempo(carrera.getTiempoEsperado());
+        return costeTotal > COSTE_MINIMO ? costeTotal : COSTE_MINIMO;
+    }
 }
