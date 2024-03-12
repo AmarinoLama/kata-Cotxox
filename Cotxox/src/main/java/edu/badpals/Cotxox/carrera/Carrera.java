a5394c2b18a24ee2a9535164195e28b1d1f2e319
AmarinoLama <amarinolama@gmail.com>
2024-03-12 14:07:36 +0100

    feat (carrera) : add atributtes

--- Cotxox/src/main/java/edu/badpals/Cotxox/carrera/Carrera.java
+++ Cotxox/src/main/java/edu/badpals/Cotxox/carrera/Carrera.java
@@ -2,6 +2,13 @@ package edu.badpals.Cotxox.carrera;
 
 public class Carrera {
 
-
+    private String tarjetaCredito;
+    private String origen;
+    private String destino;
+    private double distancia;
+    private int tiempoEsperado;
+    private int tiempoCarrera;
+    private double costeTotal;
+    private int propina;
 
 }
